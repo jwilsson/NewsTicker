76b16ed7f6a5761c11d262c8d6c98a2db1fbda96
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2011-04-15 14:12:56 +0200

    Updated to version 1.0.3

--- jquery.newsticker.js
+++ jquery.newsticker.js
@@ -1,8 +1,8 @@
 /**
- * jQuery NewsTicker 1.0.2
+ * jQuery NewsTicker 1.0.3
  * http://jonathanwilsson.com/projects/jquery-newsticker/
  *
- * Copyright 2010 Jonathan Wilsson
+ * Copyright 2011 Jonathan Wilsson
  *
  * Original code by Nicolas Gutierrez
  * http://www.yourinspirationweb.com/en/jquery-how-to-create-a-news-ticker-with-just-a-few-javascript-lines/
@@ -13,30 +13,35 @@
 
 (function ($) {
 
-	$.fn.NewsTicker = function (options) {
+    $.fn.NewsTicker = function (options) {
 
-		var defaults = {
-			interval: 5000
-		};
-
-		return this.each(function () {
-
-			var $this = $(this), id = '#' + $this.attr('id'), ticker_height = $this.height();
-
-			// If the user has supplied options let's merge them with the defaults
-			if (options) {
-				$.extend(defaults, options);
-			}
-
-			// Change the text and the specified interval
-			setInterval(function () {
-				$(id + ' li:first').animate({marginTop: ticker_height}, 800, function () {
-					$(this).detach().appendTo(id).removeAttr('style');
-				});
-			}, defaults.interval);
-
-		});
-
+	var defaults = {
+	    interval: 5000
 	};
+
+	return this.each(function () {
+
+	    var $this = $(this), id = '#' + $this.attr('id'), ticker_height = $this.height();
+
+	    // If the user has supplied options let's merge them with the defaults
+	    if (options) {
+		$.extend(defaults, options);
+	    }
+			
+	    // Change the text and the specified interval
+	    function tick () {
+		setTimeout(function () {
+		    $(id + ' li:first').animate({marginTop: ticker_height}, 800, function () {
+			$(this).detach().appendTo(id).removeAttr('style');
+		    });
+				
+		    tick();
+		}, defaults.interval);
+	    }
+			
+	    tick();
+	});
+	    
+    };
 
 }(jQuery));
